68cd07e5af35f5ea0b4ad22f4ab5ce698a62d49b
shone26 <153415158+shone26@users.noreply.github.com>
2024-10-29 11:26:10 +0530

    main

---
+++ FrontEnd/src/app/listings-page/listings-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listings-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './listings-page.component.html',
+  styleUrl: './listings-page.component.css'
+})
+export class ListingsPageComponent {
+
+}
