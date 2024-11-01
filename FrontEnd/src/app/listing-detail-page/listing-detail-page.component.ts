064aa8a4685b3ba9a561a3b796b5678e6ebe4fa5
shone26 <153415158+shone26@users.noreply.github.com>
2024-11-01 21:15:31 +0530

    listing-detail-page done
    in the listing page everything ok. also it is linked to the contact page also..
    make some edits on listing-detail-page.component.ts also

--- FrontEnd/src/app/listing-detail-page/listing-detail-page.component.ts
+++ FrontEnd/src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listing-detail-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './listing-detail-page.component.html',
   styleUrl: './listing-detail-page.component.css'
 })
-export class ListingDetailPageComponent {
+export class ListingDetailPageComponent implements OnInit {
+  listing: Listing | undefined;
 
+  constructor(
+    private route:  ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
+  }
 }
