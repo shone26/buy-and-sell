747c0d4b143a27643e8c353a17bd6e762a5ccad2
shone26 <153415158+shone26@users.noreply.github.com>
2024-11-01 23:00:17 +0530

    create my-listings-page

--- FrontEnd/src/app/my-listings-page/my-listings-page.component.ts
+++ FrontEnd/src/app/my-listings-page/my-listings-page.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-my-listings-page',
   standalone: true,
-  imports: [],
+  imports: [NgFor,RouterLink],
   templateUrl: './my-listings-page.component.html',
   styleUrl: './my-listings-page.component.css'
 })
 export class MyListingsPageComponent {
+  listings: Listing[] = [];
+
+  ngOnInit(): void {
+    this.listings = fakeListings;
+
+  }
+
+  onDeleteClicked(listingId:string): void {
+    alert(`Deleting your listing with id ${listingId}`);
+  }
 
 }
