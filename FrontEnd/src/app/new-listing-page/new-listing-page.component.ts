e1499cf3c7ba4eb5d615fcf5d6acc6f69d394696
shone26 <153415158+shone26@users.noreply.github.com>
2024-11-01 23:28:40 +0530

    create a new listing page

--- FrontEnd/src/app/new-listing-page/new-listing-page.component.ts
+++ FrontEnd/src/app/new-listing-page/new-listing-page.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-listing-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-listing-page.component.html',
   styleUrl: './new-listing-page.component.css'
 })
 export class NewListingPageComponent {
+  name: string = '';
+  description: string = '';
+  price: string = '';
+
+  constructor(
+    private router: Router,
+
+  ){}
+
+  onSubmit(): void {
+    alert('Creating a new listing....');
+    this.router.navigateByUrl('/my-listings')
+  }
 
 }
