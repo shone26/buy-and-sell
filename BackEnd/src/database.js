63c33aa6d86659af0132d33b8f1d5bc3b1a1ce19
shone26 <153415158+shone26@users.noreply.github.com>
2024-11-04 16:01:32 +0530

    connect to a mysql database from node

---
+++ BackEnd/src/database.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql';
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'hapi-server',
+    password: 'abc123!',
+    database: 'buy-and-sell',
+})
+
+export const db = {
+    connect: () => connection.connect(),
+    query: (queryString, escapedValues) =>
+        new Promise((resolve, reject) => {
+            connection.query(queryString, escapedValues, (error, results, fields) =>{
+                if (error) reject(error);
+                resolve({ results, fields });         //in here convert the mysql fuction into promises
+            })
+        }),
+
+    end: () => connection.end(),
+}
--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,14 +1,18 @@
 import Hapi from '@hapi/hapi';
 import routes from './routes';
+import { db } from './database';
+
+let server;
 
 const start = async () => {
-    const server = Hapi.server({
+    server = Hapi.server({
         port: 8000,
         host: 'localhost',
     });
 
     routes.forEach(route => server.route(route));
 
+    db.connect();
     await server.start();
     console.log(`Server is listening on ${server.info.uri}`);
 }   
@@ -17,5 +21,16 @@ process.on('unhandledRejection', err => {
     console.log(err);
     process.exit(1);
 });
+
+process.on('SIGINT', async () =>{
+    console.log('Stopping server...');
+    
+    await server.stop({ timeout: 10000 });
+    db.end();
+    console.log('Server stopped...');
+    process.exit(0);
+});  //mention when the ctrl+c (server kill) what happens to the server
+
+
 
 start();
